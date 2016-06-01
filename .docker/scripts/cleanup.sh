6e6a9682948f5678266dd78362cd376a9ccab922
nc-lex <lex@lex-imac.blc-hq.com>
2016-06-01 16:04:20 -0500

    Fixed bugs in environment variables and database connections

--- .docker/scripts/cleanup.sh
+++ .docker/scripts/cleanup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-#Make sure that exited containers are deleted.
+# Cleans up unnecessary Docker images and containers
+
+# Make sure that exited containers are deleted.
 docker rm -v $(docker ps -a -q -f status=exited)
 
-#Remove unwanted ‘dangling’ images.  
+# Remove unwanted ‘dangling’ images.  
 docker rmi $(docker images -f "dangling=true" -q)
