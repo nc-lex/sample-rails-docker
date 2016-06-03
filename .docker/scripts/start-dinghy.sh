eef888a3248899ae021cef740fbde9a760224583
nc-lex <lex@lex-imac.blc-hq.com>
2016-06-03 15:57:42 -0500

    Generalize scripts for dinghy env

--- .docker/scripts/start-dinghy.sh
+++ .docker/scripts/start-dinghy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DOCKER_HOST=tcp://192.168.99.101:2376
+export DOCKER_HOST=tcp://$(dinghy ip):2376
 export DOCKER_CERT_PATH=~/.docker/machine/machines/dinghy
 export DOCKER_TLS_VERIFY=1
 export DOCKER_MACHINE_NAME=dinghy
