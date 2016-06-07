73b8f675d9ee6fa1556a1d2bf977c5d2e626b4b5
nc-lex <lex@lex-imac.blc-hq.com>
2016-06-07 14:13:05 -0500

    Added scripts for container starting options

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
-# Initialize the database
-INIT_FILE=tmp/init
-if [ ! -f $INIT_FILE ]; then
-  .docker/wait-for-it.sh -h $INFO_DATABASE_HOST -p $INFO_DATABASE_PORT -t 30
-  if [ $? == "0" ]; then
-    echo "Initializing Database..."
+waitBash() {
+  trap "startServer" SIGTERM
+  while true ; do sleep 1 ; done
+}
 
-    bundle exec rake db:create
-    bundle exec rake db:migrate
+startServer() {
+  # Initialize the database
+  INIT_FILE=tmp/init
+  if [ ! -f $INIT_FILE ]; then
+    .docker/wait-for-it.sh -h $INFO_DATABASE_HOST -p $INFO_DATABASE_PORT -t 30
+    if [ $? == "0" ]; then
+      echo "Initializing Database..."
 
-    touch $INIT_FILE
+      bundle exec rake db:create
+      bundle exec rake db:schema:load
+
+      touch $INIT_FILE
+    fi
   fi
-fi
 
-# Remove server pid file that may be left by an unexpected shutdown
-rm tmp/pids/server.pid
+  # Remove server pid file that may be left by an unexpected shutdown
+  rm -f tmp/pids/server.pid
 
-# Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec rails s -p 3000 -b 0.0.0.0
+  echo "Starting server..."
+  # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
+  exec bundle exec rails s -p 3000 -b 0.0.0.0
+}
+
+trap "waitBash" SIGUSR1
+sleep 3
+
+startServer
