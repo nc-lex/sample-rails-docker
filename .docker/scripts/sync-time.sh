c9a180355c44eb8dec361076521c450aac968004
nc-lex <lex@lex-imac.blc-hq.com>
2016-06-17 16:38:09 -0500

    Created a base image for dev environment

---
+++ .docker/scripts/sync-time.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+MACHINE_NAME="dinghy"
+while [[ $# > 0 ]]
+do
+  key="$1"
+  case $key in
+    *)
+      MACHINE_NAME="$2"
+      shift
+    ;;
+  esac
+  shift
+done
+
+docker-machine ssh $MACHINE_NAME 'sudo ntpclient -s -h pool.ntp.org'
