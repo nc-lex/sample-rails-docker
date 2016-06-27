34209de0f0be7e49ea4d337653d69494a6d25e8d
nc-lex <lex@lex-imac.blc-hq.com>
2016-06-27 09:44:32 -0500

    Added scripts help page

--- .docker/scripts/install-dinghy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install dinghy, a custom virtual machine for Docker to run on Mac OS X
-brew tap codekitchen/dinghy
-brew install dinghy
-
-# Start dinghy virtual machine
-dinghy create --provider virtualbox
